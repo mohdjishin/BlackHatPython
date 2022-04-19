a45962398926d1614318291bd22013a4928134fb
mohdjishin <whoistcp@protonmail.com>
2022-04-19 15:56:42 -0400

    TCP proxy V0.2  facing some issue with http proxy

--- chapter_01/TCP_proxy.py
+++ chapter_01/TCP_proxy.py
@@ -25,11 +25,9 @@ def hexdump(src, length=16, show=True):
         return results
     
     
-    #------------
-    
 def receive_from(connection):
     buffer = b""
-    connection.settimeout(5)
+    connection.settimeout(20)
     try:
         while True:
             data = connection.recv(4096)
@@ -52,8 +50,82 @@ def response_handler(buffer):
     return buffer
 
 
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     remote_socket.connect((remote_host, remote_port))
+     
+     if receive_first:
+         remote_buffer = receive_from(remote_socket)
+
+         if len(remote_buffer):
+             print("[<==] received %d bytes to localhost." % len(remote_buffer))
+             hexdump(remote_buffer)
+             
+             remote_buffer = response_handler(remote_buffer)
+             client_socket.send(remote_buffer)
+             print("[==>] sent to local")
+             
+             
+     while True:
+         local_buffer = receive_from(client_socket)
+         if len(local_buffer):
+             line = "[<==]Received %d bytes from localhost." % len(local_buffer)
+            
+             hexdump(local_buffer)
+             
+             local_buffer = request_handler(local_buffer)
+             remote_socket.send(local_buffer)
+             print("[==>] Sent to remote.")
+         
+         remote_buffer = receive_from(remote_socket)
+         if len(remote_buffer):
+             print("[<==] Received %d bytes from remote." % len(remote_buffer))
+             hexdump(remote_buffer)
+             remote_buffer = response_handler(remote_buffer)
+             client_socket.send(remote_buffer)
+             print("[==>] Sent to local.")
+             
+         if not len(local_buffer) or not len(remote_buffer): 
+             client_socket.close()
+             remote_socket.close()
+             print("[*] No more data. Closing connections.")
+             break
+
+# server_loop
 
 
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    
+    except Exception as e:
+        print('problem on bind: %r' % e)
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
+    
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+         # print out the local connection information
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+
+ # start a thread to talk to the remote host
+        proxy_thread = threading.Thread( 
+            target=proxy_handler,
+            args=(client_socket, remote_host,
+                    remote_port, receive_first))
+        proxy_thread.start()
+
+
+
+
+
+# ----------------
 def main():
      if len(sys.argv[1:]) != 5:
          print("Usage: ./proxy.py [localhost] [localport]", end='')
@@ -62,8 +134,12 @@ def main():
          sys.exit(0)
      local_host = sys.argv[1]
      local_port = int(sys.argv[2])
+     
+     
      remote_host = sys.argv[3]
      remote_port = int(sys.argv[4])
+     
+     
      receive_first = sys.argv[5]
  
      if "True" in receive_first:
@@ -76,3 +152,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# command sudo python3 TCP_proxy.py 127.0.0.1 80  192.168.1.1 80 True
