3e3cf4373237374dfba48b95bffe5eb0bbf50670
mohdjishin <whoistcp@protonmail.com>
2022-05-02 14:31:03 -0400

    added first part decoder

---
+++ chapter_02/decoder.py
@@ -0,0 +1,26 @@
+from email import header
+import ipaddress
+import os
+import socket
+import struct
+import sys
+
+from requests import head
+
+# https://docs.python.org/3/library/struct.html
+class IP:
+    def __init__(self,buff=None):
+        header = struct.unpack('<BBHHHBBH4s4s', buff)
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
+
+        self.tos = header[1] 
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
+
